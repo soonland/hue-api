5a32214471a097a05e11744d9f51a5037cd39f51
soonland <76574139+soonland@users.noreply.github.com>
2022-02-26 17:44:28 -0500

    chore: winston logging

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const expressWinston = require('express-winston');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const routes = require('./routes');
-const getBridges = require('./utils/discover');
 
+const routes = require('./routes');
+const httpLogger = require('./utils/httpLogger');
+
+const logger = require('./utils/logger')(__filename);
+// const getBridges = require('./utils/discover');
+expressWinston.requestWhitelist.push('body');
+expressWinston.responseWhitelist.push('body');
 const app = express();
 
 app.use(cors());
@@ -14,13 +20,16 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(httpLogger(logger));
+
 app.use('/', routes);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 5000;
 // app.use('/api/products', productsRouter);
 
-getBridges();
+// getBridges();
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  logger.info(`Server is running on port ${PORT}.`);
 });
---
+++ src/utils/logger.js
@@ -0,0 +1,9 @@
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  level: 'debug',
+  format: format.combine(format.colorize(), format.timestamp(), format.simple()),
+  transports: [new transports.Console()],
+});
+
+module.exports = (parOrigin) => logger.child({ origin: parOrigin });
