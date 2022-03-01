cdcda2f555f44747f61a86a7bc5f950aca37818a
soonland <76574139+soonland@users.noreply.github.com>
2022-03-01 17:30:10 -0500

    feat: search new lights

--- src/controllers/bridges.js
+++ src/controllers/bridges.js
@@ -1,3 +1,4 @@
+const v3 = require('node-hue-api');
 const getBridges = require('../utils/discover');
 
 const getAllBridges = async (req, res) => {
@@ -8,4 +9,36 @@ const getAllBridges = async (req, res) => {
     });
 };
 
-module.exports = { getAllBridges };
+const getSearchNewLights = async (req, res) => {
+  const headers = '-6QQKPLW2a6LLQolgJRoVCO3wwx3C3BlhjzhEHva';
+
+  v3.discovery
+    .nupnpSearch()
+    .then((searchResults) => {
+      const host = searchResults[0].ipaddress;
+      return v3.api.createLocal(host).connect(headers);
+    })
+    .then((api) => api.lights.searchForNew())
+    .then((data) => res.send(data.data))
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+const getNewLights = async (req, res) => {
+  const headers = '-6QQKPLW2a6LLQolgJRoVCO3wwx3C3BlhjzhEHva';
+
+  v3.discovery
+    .nupnpSearch()
+    .then((searchResults) => {
+      const host = searchResults[0].ipaddress;
+      return v3.api.createLocal(host).connect(headers);
+    })
+    .then((api) => api.lights.getNew())
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+module.exports = { getAllBridges, getSearchNewLights, getNewLights };
--- src/routes/bridges.js
+++ src/routes/bridges.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const { getAllBridges } = require('../controllers/bridges');
+const { getAllBridges, getSearchNewLights, getNewLights } = require('../controllers/bridges');
 
 router.get('/', getAllBridges);
+router.get('/searchNewLights', getSearchNewLights);
+router.get('/newLights', getNewLights);
 
 module.exports = router;
