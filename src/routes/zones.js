04d3246f807dd12dd73a1d0f90f64f53147d0cc1
soonland <76574139+soonland@users.noreply.github.com>
2022-04-10 19:40:23 -0400

    feat: adding new zone

--- src/routes/zones.js
+++ src/routes/zones.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const { getAllZones, setState } = require('../controllers/zones');
+const { getAllZones, setState, addNewZone, deleteZone } = require('../controllers/zones');
 
 router.get('/', getAllZones);
+router.post('/', addNewZone);
+router.delete('/:zoneId', deleteZone);
 router.post('/:zoneId', setState);
 
 module.exports = router;
