d7239999af39422fe5ebaf2ff197d38773bfdaf5
soonland <76574139+soonland@users.noreply.github.com>
2023-02-25 12:32:02 -0500

    feat: add new routes for weather

---
+++ src/controllers/weather.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+const https = require('https');
+const { getConfiguration } = require('../utils/discover');
+
+const updateWeather = async (req, res) => {
+  getConfiguration()
+    .then((data) => data[0].internalipaddress)
+    .then(async (ipAddress) => {
+      const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+      //       const httpsAgent = new https.Agent({
+      //         cert: `-----BEGIN CERTIFICATE-----
+      // MIICMjCCAdigAwIBAgIUO7FSLbaxikuXAljzVaurLXWmFw4wCgYIKoZIzj0EAwIw
+      // OTELMAkGA1UEBhMCTkwxFDASBgNVBAoMC1BoaWxpcHMgSHVlMRQwEgYDVQQDDAty
+      // b290LWJyaWRnZTAiGA8yMDE3MDEwMTAwMDAwMFoYDzIwMzgwMTE5MDMxNDA3WjA5
+      // MQswCQYDVQQGEwJOTDEUMBIGA1UECgwLUGhpbGlwcyBIdWUxFDASBgNVBAMMC3Jv
+      // b3QtYnJpZGdlMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEjNw2tx2AplOf9x86
+      // aTdvEcL1FU65QDxziKvBpW9XXSIcibAeQiKxegpq8Exbr9v6LBnYbna2VcaK0G22
+      // jOKkTqOBuTCBtjAPBgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBhjAdBgNV
+      // HQ4EFgQUZ2ONTFrDT6o8ItRnKfqWKnHFGmQwdAYDVR0jBG0wa4AUZ2ONTFrDT6o8
+      // ItRnKfqWKnHFGmShPaQ7MDkxCzAJBgNVBAYTAk5MMRQwEgYDVQQKDAtQaGlsaXBz
+      // IEh1ZTEUMBIGA1UEAwwLcm9vdC1icmlkZ2WCFDuxUi22sYpLlwJY81Wrqy11phcO
+      // MAoGCCqGSM49BAMCA0gAMEUCIEBYYEOsa07TH7E5MJnGw557lVkORgit2Rm1h3B2
+      // sFgDAiEA1Fj/C3AN5psFMjo0//mrQebo0eKd3aWRx+pQY08mk48=
+      // -----END CERTIFICATE-----`,
+      //       });
+
+      const weatherAPI = `https://api.open-meteo.com/v1/gem?latitude=48.45&longitude=-68.52&current_weather=true&timezone=America%2FNew_York`;
+      const weather = await axios.get(weatherAPI);
+      console.log(weather.data);
+
+      const headers = { 'hue-application-key': process.env.HUE_KEY };
+      const zones = await axios.get(`https://${ipAddress}/clip/v2/resource/zone`, { httpsAgent, headers });
+      res.send(zones.data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+module.exports = { updateWeather };
